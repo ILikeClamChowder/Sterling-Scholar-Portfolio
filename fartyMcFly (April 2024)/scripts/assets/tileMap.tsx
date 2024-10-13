4b5858eb7e71fd67687604a244cc6f9fd6dbf32a
Tate Bell <114866539+ILikeClamChowder@users.noreply.github.com>
2024-10-13 12:00:27 -0600

    Delete fartyMcFly (April 2024) directory

--- fartyMcFly (April 2024)/scripts/assets/tileMap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileMap" tilewidth="18" tileheight="18" tilecount="400" columns="20">
- <image source="tileMap.png" width="360" height="360"/>
-</tileset>
